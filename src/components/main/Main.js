3fb199861a301962bde2ac1c1edb0a8dfbd6de6e
Oscar Nilsson <sawariz0r@gmail.com>
2019-04-24 12:53:25 +0200

    added logopng

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -4,12 +4,12 @@ import styles from "./Main.module.css"
 function Main() {
   return (
     <div>
-      <div className={styles.topBar}></div>
+      <div className={styles.topBar}>x</div>
       <div className={styles.mainContainer}>
-      
+      x
       </div>
     </div>
   )
 }
 
-export default Main
+export default Main;
