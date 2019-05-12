64ce2ce40ff1a65a187e06289a8e6f79311a59ab
ibrahim yolbir <ibrahimyolbirr@gmail.com>
2019-05-12 19:29:18 +0200

    login-style

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import { Dropbox } from 'dropbox';
 import { token$ } from "./store";
 import styles from "./css/Login.module.css"
 import dropboxLogo from "../dropbox-logo.png";
-
+import logo from "../logo.png";
 const CLIENT_ID = '2k9z1293u11it1b';
 
 const Login = () => {
@@ -15,6 +15,10 @@ const Login = () => {
         <>
         {token$.value ? null : 
             <div className={styles.loginBody}>
+            <header className={styles.loginHeader}>
+            <img src={logo} className={styles.logo} alt=""/>
+            <i class="fas fa-bars"></i>
+                </header>
                 <a className={styles.loginA}href={dbx.getAuthenticationUrl('http://localhost:3000/auth')}>
                 <img src={dropboxLogo} className={styles.dropboxLogo} alt=""/>
                 <button className={styles.loginButton}>Connect w/ Dropbox</button>
