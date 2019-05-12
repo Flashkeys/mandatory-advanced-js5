a155e3dba3b5ac35bc31492b6def034bdc6ada94
ibrahim yolbir <ibrahimyolbirr@gmail.com>
2019-05-12 14:19:24 +0200

    style for loginPage

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Dropbox } from 'dropbox';
+import { token$ } from "./store";
+import styles from "./css/Login.module.css"
+import dropboxLogo from "../dropbox-logo.png";
+
 const CLIENT_ID = '2k9z1293u11it1b';
 
 const Login = () => {
@@ -9,9 +13,14 @@ const Login = () => {
     })
     return (
         <>
-            <div className="login">
-                <a href={dbx.getAuthenticationUrl('http://localhost:3000/auth')}><button>Login</button></a>
+        {token$.value ? null : 
+            <div className={styles.loginBody}>
+                <a className={styles.loginA}href={dbx.getAuthenticationUrl('http://localhost:3000/auth')}>
+                <img src={dropboxLogo} className={styles.dropboxLogo} alt=""/>
+                <button className={styles.loginButton}>Connect w/ Dropbox</button>
+                </a>
             </div>
+            }
         </>
     )
 }
