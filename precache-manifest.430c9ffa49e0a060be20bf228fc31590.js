53246088724d42905d07e7c38d4dd08d8eb2d6f5
Malin Larsson <malin.larsson@utb.ecutbildning.se>
2019-05-14 15:24:38 +0200

    Updates

---
+++ precache-manifest.430c9ffa49e0a060be20bf228fc31590.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27be89bd56e12ad9c8e156c730b7dfca",
+    "url": "/mandatory-advanced-js5/index.html"
+  },
+  {
+    "revision": "8c9bc111ed5f33808f60",
+    "url": "/mandatory-advanced-js5/static/css/main.17d7be74.chunk.css"
+  },
+  {
+    "revision": "ddcaf08cd79bdd85433a",
+    "url": "/mandatory-advanced-js5/static/js/2.0b28f66f.chunk.js"
+  },
+  {
+    "revision": "8c9bc111ed5f33808f60",
+    "url": "/mandatory-advanced-js5/static/js/main.359f2c27.chunk.js"
+  },
+  {
+    "revision": "693ed353e3382e7179ea",
+    "url": "/mandatory-advanced-js5/static/js/runtime~main.d0f14806.js"
+  },
+  {
+    "revision": "19daac3e25d11675a42b6a67a0f95ead",
+    "url": "/mandatory-advanced-js5/static/media/dropbox-logo.19daac3e.png"
+  },
+  {
+    "revision": "37da5cfd8887b0b5aae72b10b8d6d714",
+    "url": "/mandatory-advanced-js5/static/media/logo.37da5cfd.png"
+  }
+]);
