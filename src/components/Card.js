1e32a10ce6b6d527fc80c395d34ff5e081797b20
Oscar Nilsson <sawariz0r@gmail.com>
2019-05-14 10:13:53 +0200

    added error handling in getThumbnail

--- src/components/Card.js
+++ src/components/Card.js
@@ -21,7 +21,9 @@ const Card = (props) => {
         .then((res) => {
             const thumb = URL.createObjectURL(res.fileBlob);
             imgRef.current.src = thumb;
-
+        })
+        .catch(err => {
+          console.log(err);
         })
   };
 
